c6d4008f22469a389bd57198d34377a681e0511d
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2023-11-02 22:33:40 -0400

    clean up

--- src/reactQueryCustomHooks.jsx
+++ src/reactQueryCustomHooks.jsx
@@ -1,13 +1,12 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import customFetch from './utils';
-import { keys } from '../keys';
 
 export const useFetchImages = () => {
   const { isLoading, data, isError, error } = useQuery({
     queryKey: ['images'],
     queryFn: async () => {
       const { data } = await customFetch.get(
-        `/?client_id=${keys.accessKey}&query=office`
+        `/?client_id=${import.meta.env.VITE_API_KEY}&query=office`
       );
       return { data, isLoading, isError, error };
     },
